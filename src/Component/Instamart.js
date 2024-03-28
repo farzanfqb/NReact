d8bbd8bd77b53a8fe20053cbe5960cf8de18bc3e
farzanfqb <farzanfqb@gmail.com>
2024-03-28 16:16:25 +0530

    Update Instamart.js
    parent controls multiple accordion state in an object and sends  in prop a particular property of an object as true or false and setState function where it is invoked in child but sets state in parent (Lifting the state up)

--- src/Component/Instamart.js
+++ src/Component/Instamart.js
@@ -1,21 +1,43 @@
 import React from 'react'
-const Section = ({ title }) => {
-  const [isOpen, setIsOpen] = React.useState(false);
+const Section = ({ title, isOpen, setIsOpen }) => {
+  console.log("open", isOpen);
   return (
-    <><h1> {title}</h1>;
+    <div><h1> {title}</h1>
       {isOpen && <p>Thank you for your kind words! It was an absolute pleasure to meet the Eonyx team and get started on our training sessions with you all. We appreciate your attention and enthusiasm as we covered the Queries module today. As noted in our session, we have created a Google Drive folder comprised of session recordings and documentation to assist in your growth of Slate knowledge. Please feel free to share the link amongst your team. We will continue to add to this folder after each training session.
         I will touch base later this week to add a few more sessions to the calendar before April 15. In the meantime, please reach out with any questions. We look forward to connecting with you all later this week and meeting again next week!</p>
-      }
-      <button onClick={() => setIsOpen(true)}>Show</button>
-      <button onClick={() => setIsOpen(false)}>Hide</button>
-    </>)
+      }<br />
+      <button onClick={() => setIsOpen()}>Show</button>
+      <button onClick={() => setIsOpen()}>Hide</button>
+    </div>)
 }
 const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = React.useState({
+    showAbout: false,
+    showTeam: false,
+    showCareers: false,
+  })
+  console.log(sectionConfig);
   return (
-    <><Section title={'Accordion1'} />
-      <Section title={'Accordion2'} />
-      <Section title={'Accordion3'} />
-    </>
+
+    <div>
+      <h1>Instamart</h1>
+      <Section title={'Show About'} isOpen={sectionConfig.showAbout} setIsOpen={() => setSectionConfig({
+        showAbout: true,
+        showTeam: false,
+        showCareers: false,
+      })} />
+      <Section title={'Show Team'} isOpen={sectionConfig.showTeam} setIsOpen={() => setSectionConfig({
+        showAbout: false,
+        showTeam: true,
+        showCareers: false,
+      })} />
+      <Section title={'Show Careers'} isOpen={sectionConfig.showCareers} setIsOpen={() => setSectionConfig({
+        showAbout: false,
+        showTeam: false,
+        showCareers: true,
+      })} />
+    </div>
+
   )
 }
 
